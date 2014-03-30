8f67471c6234acf3184e8248bc4d581374f18036
yoojin529 <yoojin529@naver.com>
2014-03-30 18:49:37 +0900

    button만들기
    button을 누르면 text가 바뀜

--- exercise1/src/com/example/exercise1/MainActivity.java
+++ exercise1/src/com/example/exercise1/MainActivity.java
@@ -4,13 +4,27 @@ package com.example.exercise1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private TextView textview;
+	private Button button;	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		textview = (TextView)findViewById(R.id.tv_text);
+		button = (Button)findViewById(R.id.btn_button);
+
+		
+		button.setOnClickListener(this);
 	}
 
 	@Override
@@ -19,5 +33,14 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void onClick(View v) {
+		textview.setText("hi");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
